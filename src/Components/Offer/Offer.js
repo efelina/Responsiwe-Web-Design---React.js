ad62e7f75b6fcc239ab1a31897db5362b08d12ab
Ewelina Herec <ewe.herec@gmail.com>
2020-08-26 18:13:35 +0200

    add offerBoxes array, add OfferBox.css

--- src/Components/Offer/Offer.js
+++ src/Components/Offer/Offer.js
@@ -2,6 +2,53 @@ import React from 'react';
 import './Offer.css';
 import OfferBox from './OfferBox/OfferBox';
 
+const offerBoxes = [
+    {
+        name:"Usługa 1",
+        isNew: true
+    },
+
+    {
+        name:"Usługa 2",
+        isNew: false
+    },
+
+    {
+        name:"Usługa 3",
+        isNew: true
+    },
+
+    {
+        name:"Usługa 4",
+        isNew: false
+    },
+
+    {
+        name:"Usługa 5",
+        isNew: false
+    },
+
+    {
+        name:"Usługa 6",
+        isNew: false
+    },
+
+    {
+        name:"Usługa 7",
+        isNew: false
+    },
+
+    {
+        name:"Usługa 8",
+        isNew: false
+    },
+
+    {
+        name:"Usługa 9",
+        isNew: false
+    }
+
+]
 
 function Offer() {
     return (
@@ -10,7 +57,11 @@ function Offer() {
                 <h1>Czym zajmuje się nasza firma? </h1>
                 <div className="centering-box">
                     <div className="box-container">
-                        <OfferBox OfferName={`Usługa 1`} isNew={true} />                       
+                    {
+                        offerBoxes.map(offerBox => {
+                          return <OfferBox OfferName={offerBox.name} isNew={offerBox.isNew}/>  
+                        })
+                    }                       
                     </div>
                 </div>
 
